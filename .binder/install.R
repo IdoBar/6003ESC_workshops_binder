afa31427144095881f614a56529fe220fc575d20
Ido Bar <ido.idobar@gmail.com>
2021-03-29 12:11:36 +1000

    Initialise repository with all workshops

---
+++ .binder/install.R
@@ -0,0 +1,7 @@
+install.packages(c(
+  "tidyverse", "pacman", "shiny", "rmarkdown", "ggpubr", "paletteer",
+  "learnr",  "scales", "captioner", "here", "htmltools", "fontawesome",  
+  "agricolae", "car", "MASS", "highcharter", "plotly", "lubridate",
+  "ISOweek", "countrycode")
+)
+
