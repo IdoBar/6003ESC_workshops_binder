74ac27c7c13275a4810621462606488a8d39139c
Ido Bar <ido.idobar@gmail.com>
2021-03-29 22:12:34 +1000

    Fix pre-installation of tweetrmd

--- .binder/install.R
+++ .binder/install.R
@@ -1,7 +1,7 @@
-install.packages(c("tweetrmd", "pandoc", 
+install.packages(c("remotes", "pandoc", 
   "tidyverse", "pacman", "shiny", "rmarkdown", "ggpubr", "paletteer",
   "learnr",  "scales", "captioner", "here", "htmltools", "fontawesome",  
   "agricolae", "car", "MASS", "highcharter", "plotly", "lubridate",
   "ISOweek", "countrycode")
 )
-
+remotes::install_github("gadenbuie/tweetrmd")
